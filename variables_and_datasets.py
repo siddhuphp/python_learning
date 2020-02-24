cf2946b599b7d917593607667c1f34d2719e925e
siddhuphp <siddhu.php@gmail.com>
2020-02-24 17:49:40 +0530

    learned variables decleraction

---
+++ variables_and_datasets.py
@@ -0,0 +1,11 @@
+message = 'Welcome to variables declaration'
+print(message)
+
+message = "with double quotes"
+print(message)
+
+message = 'I told my friend, "Python is my favorite language!"'
+print(message)
+
+message = "One of Python's strengths is its diverse and supportive community."
+print(message)
